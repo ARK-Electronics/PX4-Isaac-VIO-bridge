7b7af6f5db3b189ca75ba5ff2c8bd445824377c1
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-11-22 13:39:13 -0900

    added status publishing

--- src/vio_transform.cpp
+++ src/vio_transform.cpp
@@ -1,8 +1,9 @@
 #include <rclcpp/rclcpp.hpp>
-#include <px4_msgs/msg/vehicle_odometry.hpp>
-#include <nav_msgs/msg/odometry.hpp>
 #include "tf2/LinearMath/Matrix3x3.h"
 #include "tf2/LinearMath/Transform.h"
+#include <px4_msgs/msg/vehicle_odometry.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include "isaac_ros_visual_slam_interfaces/msg/visual_slam_status.hpp"
 
 class VioTransform : public rclcpp::Node
 {
@@ -17,14 +18,32 @@ explicit VioTransform() : Node("vio_transform")
 
 	_vslam_sub = this->create_subscription<nav_msgs::msg::Odometry>("/visual_slam/tracking/odometry", qos,
 		std::bind(&VioTransform::publish, this, std::placeholders::_1));
+
+	_vslam_status_sub = this->create_subscription<isaac_ros_visual_slam_interfaces::msg::VisualSlamStatus>("/visual_slam/status", qos,
+		std::bind(&VioTransform::statusCallback, this, std::placeholders::_1));
 }
 
 private:
 	void publish(const nav_msgs::msg::Odometry::UniquePtr msg);
+	void statusCallback(const isaac_ros_visual_slam_interfaces::msg::VisualSlamStatus::UniquePtr msg);
 
 	rclcpp::Publisher<px4_msgs::msg::VehicleOdometry>::SharedPtr _vio_pub;
+
 	rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr _vslam_sub;
+	rclcpp::Subscription<isaac_ros_visual_slam_interfaces::msg::VisualSlamStatus>::SharedPtr _vslam_status_sub;
+
+
+	uint8_t _vslam_state = 0;
 };
+
+void VioTransform::statusCallback(const isaac_ros_visual_slam_interfaces::msg::VisualSlamStatus::UniquePtr msg)
+{
+	if (msg->vo_state != _vslam_state) {
+		RCLCPP_INFO(get_logger(), "[VioTransform] state change: %u", msg->vo_state);
+	}
+
+	_vslam_state = msg->vo_state;
+}
 
 void VioTransform::publish(const nav_msgs::msg::Odometry::UniquePtr msg)
 {
@@ -36,12 +55,17 @@ void VioTransform::publish(const nav_msgs::msg::Odometry::UniquePtr msg)
 	// FRD local tangent frame (x: Forward, y: Right, z: Down) with origin fixed relative to earth.
 	vio.pose_frame = vio.POSE_FRAME_FRD; // same as MAV_FRAME_LOCAL_FRD
 
+
+	// It's a yaw -90
+	// TODO: replace with a quat rotation and try to understand why it's like this
 	// Position is some fucked up coordinate system
 	tf2::Vector3 position;
 	position.setX(-msg->pose.pose.position.y);
 	position.setY(msg->pose.pose.position.x);
 	position.setZ(msg->pose.pose.position.z);
 
+	// It's a roll 90 yaw 180
+	// TODO: replace with a quat rotation and try to understand why it's like this
 	// The realsense VSLAM output is left handed.
 	// We convert RealSense frame (DLB) to PX4 frame (FRD) by:
 	tf2::Quaternion q;
@@ -58,6 +82,8 @@ void VioTransform::publish(const nav_msgs::msg::Odometry::UniquePtr msg)
 	vio.q[1] = q[1];
 	vio.q[2] = q[2];
 	vio.q[3] = q[3];
+
+	// TODO: still need to transform the covariances etc...
 
 	vio.velocity_frame = vio.VELOCITY_FRAME_FRD;
 
@@ -82,7 +108,7 @@ void VioTransform::publish(const nav_msgs::msg::Odometry::UniquePtr msg)
 	vio.velocity_variance[2] = msg->twist.covariance[14];
 
 	vio.reset_counter = 0; // TODO: look into issac_ros_vslam code to see if we can expose it
-	vio.quality = 0; // 0 = unknown/unset quality
+	vio.quality = _vslam_state; // 0 = unknown/unset quality
 
 	_vio_pub->publish(vio);
 }
