091c0a5740c9087b9a4ce1af092b0ede04d7b67c
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-11-22 15:46:49 -0900

    world frame

--- src/vio_transform.cpp
+++ src/vio_transform.cpp
@@ -26,8 +26,8 @@ explicit VioTransform() : Node("vio_transform")
 private:
 	void publish(const nav_msgs::msg::Odometry::UniquePtr msg);
 	void statusCallback(const isaac_ros_visual_slam_interfaces::msg::VisualSlamStatus::UniquePtr msg);
-	void NWU_to_FRD_position(tf2::Vector3& position);
-	void NWU_to_FRD_orientation(tf2::Quaternion& quat);
+	void NWU_to_NED_position(tf2::Vector3& position);
+	void NWU_to_NED_orientation(tf2::Quaternion& quat);
 
 	rclcpp::Publisher<px4_msgs::msg::VehicleOdometry>::SharedPtr _vio_pub;
 
@@ -47,24 +47,20 @@ void VioTransform::statusCallback(const isaac_ros_visual_slam_interfaces::msg::V
 	_vslam_state = msg->vo_state;
 }
 
-void VioTransform::NWU_to_FRD_position(tf2::Vector3& position)
+void VioTransform::NWU_to_NED_position(tf2::Vector3& position)
 {
 	tf2::Quaternion NED_NWU_Q; // = utils::quaternion::quaternion_from_euler(M_PI, 0.0, M_PI_2);
 	NED_NWU_Q.setRPY(0.0, 0.0, M_PI_2);
 	position = tf2::quatRotate(NED_NWU_Q, position);
 }
 
-void VioTransform::NWU_to_FRD_orientation(tf2::Quaternion& quat)
+void VioTransform::NWU_to_NED_orientation(tf2::Quaternion& quat)
 {
 	tf2::Quaternion NED_NWU_Q; // = utils::quaternion::quaternion_from_euler(M_PI, 0.0, M_PI_2);
 	NED_NWU_Q.setRPY(0.0, 0.0, M_PI_2);
-	tf2::Quaternion AIRCRAFT_BASELINK_Q; //  = utils::quaternion::quaternion_from_euler(M_PI, 0.0, 0.0);
-	AIRCRAFT_BASELINK_Q.setRPY(M_PI, 0.0, 0.0); // FLU to FRD
 
 	// rotate quaterion from NWU into NED
 	quat = NED_NWU_Q * quat;
-	// rotation FLU-->FRD into NED frame
-	quat = quat * AIRCRAFT_BASELINK_Q;
 }
 
 void VioTransform::publish(const nav_msgs::msg::Odometry::UniquePtr msg)
@@ -74,8 +70,7 @@ void VioTransform::publish(const nav_msgs::msg::Odometry::UniquePtr msg)
 	vio.timestamp = msg->header.stamp.sec * 1000000 + msg->header.stamp.nanosec / 1000;
 	vio.timestamp_sample = vio.timestamp;
 
-	// FRD local tangent frame (x: Forward, y: Right, z: Down) with origin fixed relative to earth.
-	vio.pose_frame = vio.POSE_FRAME_FRD; // same as MAV_FRAME_LOCAL_FRD
+	vio.pose_frame = vio.POSE_FRAME_NED;
 
 	tf2::Vector3 position;
 	position.setX(msg->pose.pose.position.x);
@@ -88,8 +83,8 @@ void VioTransform::publish(const nav_msgs::msg::Odometry::UniquePtr msg)
 	quat.setZ(msg->pose.pose.orientation.z);
 	quat.setW(msg->pose.pose.orientation.w);
 
-	NWU_to_FRD_position(position);
-	NWU_to_FRD_orientation(quat);
+	NWU_to_NED_position(position);
+	NWU_to_NED_orientation(quat);
 
 	vio.position[0] = position[0];
 	vio.position[1] = position[1];
@@ -102,7 +97,7 @@ void VioTransform::publish(const nav_msgs::msg::Odometry::UniquePtr msg)
 
 	// TODO: still need to transform the covariances etc...
 
-	vio.velocity_frame = vio.VELOCITY_FRAME_FRD;
+	vio.velocity_frame = vio.VELOCITY_FRAME_NED;
 
 	vio.velocity[0] = msg->twist.twist.linear.x;
 	vio.velocity[1] = msg->twist.twist.linear.y;
